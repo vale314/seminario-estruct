fe6fca979f0d19bfe3a7fb0da2ec9c0ebb2ba089
vale314 <electrovale314@hotmail.com>
2018-08-16 12:43:32 -0500

    1.3

--- Example_prof/main.cpp
+++ Example_prof/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+#define MAX 5
+
 struct Cancion{
     string nombre;
     string artista;
@@ -13,23 +15,91 @@ struct Cancion{
 
 int main(int argc, char *argv[])
 {
-
     Cancion primera;
+    Cancion disco[MAX];
 
-    cout << "Escriba el nombre de la cancion";
-    getline(cin,primera.nombre);
-    cout << "Escriba el nombre del artista";
-    getline(cin,primera.artista);
-    cout << "Escribe la duracion";
-    cin >> primera.duracion;
-    cout << "Escribe el anio";
-    cin >> primera.anio;
+    int canciones,r,r1;
+    string name;
+    r1=0;
+    cout << "Cuantas canciones tiene el disco";
+    cin >> canciones;
 
-    cout << " Nombre: " <<primera.nombre << endl
-        <<"Artista: " << primera.artista <<endl
-        <<"Duarcion: " << primera.duracion << endl
-        << "Genero: " <<primera.genero <<endl
-        << "Anio: " <<primera.anio << endl;
+
+    do{
+        cout << "1 Agregar Cancion" << endl;
+        cout << "2 Consultar disco" <<endl;
+        cout << "3 Buscar cancion" <<endl;
+        cout << "4 Salir" << endl;
+
+        cin >> r;
+
+        switch (r) {
+        case 1:
+            cin.ignore();
+            if(r1 >= canciones){
+                cout << "El Disco Se Encuentra Lleno" << endl;
+                break;
+            }
+                cout << "Escriba el nombre de la cancion" << endl;
+                getline(cin,disco[r1].nombre);
+                cout << "Escriba el nombre del artista" << endl;
+                getline(cin,disco[r1].artista);
+                cout << "Escribe la duracion" << endl ;
+                cin >> disco[r1].duracion;
+                cout << "Escribe el anio" << endl;
+                cin >> disco[r1].anio;
+                cin.ignore();
+                cout << "Escriba el nombre del genero" << endl;
+                getline(cin,disco[r1].genero);
+                r1++;
+            break;
+        case 2:
+            if(!r1){
+                cout << "No se encuentran canicones en el disco" << endl;
+                break;
+            }
+            for(int i =0;i< r1; i++){
+                cout << " Nombre: " <<disco[i].nombre << endl
+                    <<"Artista: " << disco[i].artista <<endl
+                    <<"Duarcion: " << disco[i].duracion << endl
+                    << "Genero: " <<disco[i].genero <<endl
+                    << "Anio: " <<disco[i].anio << endl;
+            }
+            break;
+        case 3:
+            if(!r1){
+                cout << "No se encuntran canciones" << endl;
+                break;
+            }
+                cin.ignore();
+                cout << "Por favor introdusca el nombre" << endl;
+                cin >> name;
+
+                for(int i=0; i<r1; i++){
+                    if(disco[i].nombre.compare(name) ==0){
+                        cout << " Nombre: " <<disco[i].nombre << endl
+                            <<"Artista: " << disco[i].artista <<endl
+                            <<"Duarcion: " << disco[i].duracion << endl
+                            << "Genero: " <<disco[i].genero <<endl
+                            << "Anio: " <<disco[i].anio << endl;
+                        break;
+                    }
+
+                }
+                cout << "No encontrada cancion" <<endl;
+
+            break;
+        case 4:
+
+            break;
+        default:
+            cout << " Adios";
+            break;
+        }
+
+    }while(r!=4);
+
+
 
 
     return 0;
