5f1d6bbf633edf06d8a1e01ed764da0e7629d904
vale314 <electrovale314@hotmail.com>
2018-08-16 11:52:37 -0500

    1.1

--- Example_prof/main.cpp
+++ Example_prof/main.cpp
@@ -1,8 +1,36 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Cancion{
+    string nombre;
+    string artista;
+    int duracion;
+    string genero;
+    int anio;
+};
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    return a.exec();
+    Cancion primera;
+
+    cout << "Escriba el nombre de la cancion";
+    getline(cin,primera.nombre);
+    cout << "Escriba el nombre del artista";
+    getline(cin,primera.artista);
+    cout << "Escribe la duracion";
+    cin >> primera.duracion;
+    cout << "Escribe el anio";
+    cin >> primera.anio;
+
+    cout << " Nombre: " <<primera.nombre << endl
+        <<"Artista: " << primera.artista <<endl
+        <<"Duarcion: " << primera.duracion << endl
+        << "Genero: " <<primera.genero <<endl
+        << "Anio: " <<primera.anio << endl;
+
+
+    return 0;
 }
