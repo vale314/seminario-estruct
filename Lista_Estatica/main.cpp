3922f27085814f66e9c9e00e0970d8ab9e7573e7
Your Name <you@example.com>
2018-09-04 12:37:36 -0500

    ListaEstatica

---
+++ Lista_Estatica/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "ListaEstatica.h"
+
+using namespace std;
+
+int main()
+{
+    ListaEstatica<int> enteros;
+    ListaEstatica<char> simbolos;
+
+    enteros._insert(33);
+    enteros._insert(44);
+    enteros._insert(59);
+
+    simbolos._insert('A');
+
+    cout << "Enteros empty " << enteros._empty() << endl
+         << "Enteros full " << enteros.full() << endl
+         << "Enteros Size " << enteros._size() << endl
+         <<"Simbolos empty " << simbolos._empty() << endl
+         << "Simbolos full " <<simbolos.full() << endl
+         << "Simbolos size " << simbolos._size() << endl;
+
+         enteros.print();
+         cout<< "Elemento se encontro en " << enteros._find(59) << endl;
+         enteros._remove(1);
+         enteros.print();
+
+    return 0;
+}
